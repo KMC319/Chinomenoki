5646fcf81332d9256e7d2935637a0c223a10857a
KMC319 <31245129+KMC319@users.noreply.github.com>
2020-01-27 12:23:13 +0900

    add icon

--- Chinomenoki/Assets/Scripts/Main/GameManager.cs
+++ Chinomenoki/Assets/Scripts/Main/GameManager.cs
@@ -4,10 +4,17 @@ using Data;
 using UniRx.Async;
 using UniRx.Async.Triggers;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Valve.VR.InteractionSystem;
 using Zenject;
 
 namespace Main {
     public class GameManager : MonoBehaviour {
+        private void Update() {
+            if (Input.GetKeyDown(KeyCode.R)) {
+                Scene loadScene = SceneManager.GetActiveScene();
+                SceneManager.LoadScene(loadScene.name);
+            }
+        }
     }
 }
